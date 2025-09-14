2f67552d77d3dff4785147f54cc0e578a4d39582
khaisilk1910 <3167468+khaisilk1910@users.noreply.github.com>
2025-09-14 11:40:50 +0700

    Update config_flow.py

--- custom_components/am_lich_viet_nam/config_flow.py
+++ custom_components/am_lich_viet_nam/config_flow.py
@@ -1,12 +1,11 @@
 from homeassistant import config_entries
-from .const import DOMAIN # Make sure to create a const.py or just use the string
+from .const import DOMAIN
 
 class AmLichConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
         """Handle the initial step."""
-        # Ensure only one instance of the integration can be configured
         if self._async_current_entries():
             return self.async_abort(reason="single_instance_allowed")
 
