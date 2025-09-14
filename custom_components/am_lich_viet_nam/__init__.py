7f3721d0ebfeee98e06fb96abb8f6a5b64428cec
khaisilk1910 <3167468+khaisilk1910@users.noreply.github.com>
2025-09-14 11:40:14 +0700

    Update __init__.py

--- custom_components/am_lich_viet_nam/__init__.py
+++ custom_components/am_lich_viet_nam/__init__.py
@@ -1,10 +1,14 @@
+"""The Vietnamese Lunar Calendar integration."""
 from homeassistant.core import HomeAssistant
 from homeassistant.config_entries import ConfigEntry
 
-DOMAIN = "am_lich_viet_nam"
+from .const import DOMAIN
 
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
-    hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(entry, "sensor")
-    )
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Set up Âm lịch Việt Nam from a config entry."""
+    await hass.config_entries.async_forward_entry_setup(entry, "sensor")
     return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Unload a config entry."""
+    return await hass.config_entries.async_forward_entry_unload(entry, "sensor")
