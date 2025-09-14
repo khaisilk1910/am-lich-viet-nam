59512d3f56eb827d90ad13c19c2638f96f44b7b6
khaisilk1910 <3167468+khaisilk1910@users.noreply.github.com>
2025-09-14 11:57:14 +0700

    Update __init__.py

--- custom_components/am_lich_viet_nam/__init__.py
+++ custom_components/am_lich_viet_nam/__init__.py
@@ -6,9 +6,10 @@ from .const import DOMAIN
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up Âm lịch Việt Nam from a config entry."""
-    await hass.config_entries.async_forward_entry_setups(entry, ["sensor"]) # <--- THÊM "S" VÀO CUỐI
+    await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
-    return await hass.config_entries.async_forward_entry_unload(entry, "sensor")
+    # SỬA DÒNG BÊN DƯỚI
+    return await hass.config_entries.async_forward_entry_unloads(entry, ["sensor"])
