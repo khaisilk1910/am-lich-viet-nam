d8feef789e5ea4d591a70301f3c3b647ed39bc2b
khaisilk1910 <3167468+khaisilk1910@users.noreply.github.com>
2025-09-14 11:18:23 +0700

    Update __init__.py

--- custom_components/am_lich_viet_nam/__init__.py
+++ custom_components/am_lich_viet_nam/__init__.py
@@ -1,2 +1,10 @@
+from homeassistant.core import HomeAssistant
+from homeassistant.config_entries import ConfigEntry
+
 DOMAIN = "am_lich_viet_nam"
 
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+    hass.async_create_task(
+        hass.config_entries.async_forward_entry_setup(entry, "sensor")
+    )
+    return True
