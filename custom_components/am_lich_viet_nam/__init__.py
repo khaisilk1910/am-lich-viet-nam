014ae34bfaf90e5f1b4c53b72896239ddf417934
khaisilk1910 <3167468+khaisilk1910@users.noreply.github.com>
2026-03-02 16:16:02 +0700

    Update __init__.py

--- custom_components/am_lich_viet_nam/__init__.py
+++ custom_components/am_lich_viet_nam/__init__.py
@@ -6,7 +6,6 @@ from .const import DOMAIN
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up Âm lịch Việt Nam from a config entry."""
-    # Thêm listener để lắng nghe khi người dùng thay đổi cấu hình sự kiện
     entry.async_on_unload(entry.add_update_listener(update_listener))
     
     await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
