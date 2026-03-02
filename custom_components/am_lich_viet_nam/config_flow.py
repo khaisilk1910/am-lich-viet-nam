a6a067e9ba7ef6bf60bd924b684aee31e96714b1
khaisilk1910 <3167468+khaisilk1910@users.noreply.github.com>
2026-03-02 15:03:44 +0700

    Update config_flow.py

--- custom_components/am_lich_viet_nam/config_flow.py
+++ custom_components/am_lich_viet_nam/config_flow.py
@@ -1,5 +1,9 @@
+import voluptuous as vol
 from homeassistant import config_entries
-from .const import DOMAIN
+from homeassistant.core import callback
+from homeassistant.helpers.selector import TextSelector, TextSelectorConfig, TextSelectorType
+
+from .const import DOMAIN, CONF_CUSTOM_EVENTS
 
 class AmLichConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
@@ -10,6 +14,44 @@ class AmLichConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             return self.async_abort(reason="single_instance_allowed")
 
         if user_input is not None:
-            return self.async_create_entry(title="Âm lịch Việt Nam", data={})
+            # Lưu user_input vào options để đồng nhất với OptionsFlow
+            return self.async_create_entry(title="Âm lịch Việt Nam", data={}, options=user_input)
 
-        return self.async_show_form(step_id="user")
+        return self.async_show_form(
+            step_id="user",
+            data_schema=vol.Schema({
+                vol.Optional(CONF_CUSTOM_EVENTS, default=""): TextSelector(
+                    TextSelectorConfig(type=TextSelectorType.TEXT, multiline=True)
+                )
+            })
+        )
+
+    @staticmethod
+    @callback
+    def async_get_options_flow(config_entry):
+        """Get the options flow for this handler."""
+        return AmLichOptionsFlowHandler(config_entry)
+
+class AmLichOptionsFlowHandler(config_entries.OptionsFlow):
+    """Handle options flow."""
+
+    def __init__(self, config_entry: config_entries.ConfigEntry):
+        """Initialize options flow."""
+        self.config_entry = config_entry
+
+    async def async_step_init(self, user_input=None):
+        """Manage the options."""
+        if user_input is not None:
+            return self.async_create_entry(title="", data=user_input)
+
+        # Hiện lại những sự kiện đã nhập trước đó
+        current_events = self.config_entry.options.get(CONF_CUSTOM_EVENTS, "")
+
+        return self.async_show_form(
+            step_id="init",
+            data_schema=vol.Schema({
+                vol.Optional(CONF_CUSTOM_EVENTS, default=current_events): TextSelector(
+                    TextSelectorConfig(type=TextSelectorType.TEXT, multiline=True)
+                )
+            })
+        )
