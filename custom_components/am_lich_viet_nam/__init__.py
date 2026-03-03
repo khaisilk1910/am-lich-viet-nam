8103094f61629f2e00b952c8ced1f69a00dcd279
khaisilk1910 <3167468+khaisilk1910@users.noreply.github.com>
2026-03-03 09:25:18 +0700

    Update __init__.py

--- custom_components/am_lich_viet_nam/__init__.py
+++ custom_components/am_lich_viet_nam/__init__.py
@@ -6,15 +6,9 @@ from .const import DOMAIN
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up Âm lịch Việt Nam from a config entry."""
-    entry.async_on_unload(entry.add_update_listener(update_listener))
-    
     await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
     return await hass.config_entries.async_forward_entry_unloads(entry, ["sensor"])
-
-async def update_listener(hass: HomeAssistant, entry: ConfigEntry):
-    """Handle options update."""
-    await hass.config_entries.async_reload(entry.entry_id)
